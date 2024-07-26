12e4d44bc4996d9775b1159a89539c1d4f06d0df
신정원 <js@sinjeong-wons-MacBook-Pro-2.local>
2024-07-26 15:16:09 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='detr',
+    description="DEtection TRansformers (DETR) with PyTorch",
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT License',
+    url="https://github.com/jungwonshin/train-localll.git",
+    long_description=open('README.md').read(),
+    install_requires=[],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
